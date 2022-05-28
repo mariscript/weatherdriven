6b6b09b552fb84e750e2a172496b372e73bcc3fe
Marison <103083341+mariscript@users.noreply.github.com>
2022-05-28 17:24:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+function setupDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Nove,ber",
+    "December",
+  ];
+
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  let nowDate = date.getDate();
+
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${day}, ${month} ${nowDate}, ${year}, ${hours}:${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#feels").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+// Use imperial metric, because USA
+
+function searchCity(city) {
+  let apiKey = "4f158b998cdd1f3876802fe0834b00f3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function magnifyButton(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "4f158b998cdd1f3876802fe0834b00f3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateIndex = document.querySelector("#date");
+let currentTime = new Date();
+dateIndex.innerHTML = setupDate(currentTime);
+
+let searchDoc = document.querySelector("#search-form");
+searchDoc.addEventListener("submit", magnifyButton);
+
+let pinDropButton = document.querySelector("#current-location-button");
+pinDropButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New Jersey");
